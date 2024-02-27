c4d07f827f6ea22fc1d65b519444acd6e0abc0db
inoyatovlee <inoyatovalijon02@gmail.com>
2024-02-27 22:19:43 +0500

    userProfileInfo

--- lib/pages/UserProfile.dart
+++ lib/pages/UserProfile.dart
@@ -48,27 +48,7 @@ class _UserProfileState extends State<UserProfile> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text("Username",
-                    style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 13)), //fontFamily: "Ubu"
-                Text("Category/Genre text",
-                    style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 13,
-                        color: Colors.grey.shade700)),
-                const Text.rich(TextSpan(children: [
-                  TextSpan(
-                      text:
-                          "Lorem ipsum dolor sit amet, consectentur adipiscing elit, set do eiusmod tempor insiditund"),
-                  TextSpan(
-                      text: "#hashtag", style: TextStyle(color: Colors.blue))
-                ])),
-                Text(
-                  "Link goes here",
-                  style: TextStyle(
-                      fontWeight: FontWeight.w700, color: Colors.blue.shade900),
-                ),
+                UserProfileInfo(),
                 const SizedBox(height: 12),
                 Row(
                   children: [
